598ce8f4466fa4dc6d08a6861ff6c720bd1737ad
jantiks <58886410+jantiks@users.noreply.github.com>
2021-01-21 22:25:40 +0400

    fibonachi sequence problem is solved

---
+++ src/fibonachi.java
@@ -0,0 +1,19 @@
+/*
+class lists  fibonachi sequence
+ */
+import acm.program.*;
+
+public class fibonachi extends ConsoleProgram {
+    private static final int MAX_TERM_VALUE = 10000;
+    public void run() {
+        int current = 1 ;
+        int last = 0;
+        println(last);
+        while (current < MAX_TERM_VALUE) {
+            println(current);
+            current += last;
+            last = current - last;
+
+        }
+    }
+}
